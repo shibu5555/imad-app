280691bab5a8ebeda2c4fd2a97ab8644491179ba
shibu5555 <shuvamgiri80@gmail.com>
2017-09-04 23:41:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,57 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var ArticleOne={
+	title:'The first Article',
+	heading:'Article-one',
+	date:'5 sep 2017',
+	content:`<p>its completd done its completd done its completd done its completd done its completd done its completd done its completd done its completd done</p>
+
+			<p>its completd done its completd done its completd done its completd done its completd done its completd done its completd done its completd done</p>
+				
+			<p>its completd done its completd done its completd done its completd done its completd done its completd done its completd done its completd done</p>`
+};
+
+function createTempler(data){
+	var title=data.title;
+	var date=data.date;
+	var heading=data.heading;
+	var content=data.content;
+	var htmlTemplate=`<html>
+	<head>
+		<title>
+			${title}
+		</title>
+		<link href="/ui/style.css" rel="stylesheet" />
+	</head>
+	<body>
+	<div class="container">
+			<div>
+				<h1>${heading}</h1>
+			</div>
+			<div>
+				<a href="/">Home</a>
+			</div>
+			<hr/>
+			<div>
+				${date}
+			</div>
+			<div>
+				${content}
+			</div>
+		</div>
+	</body>
+</html>
+`;	
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/Article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
+  res.send(createTempler(ArticleOne))
 });
 
 app.get('/Article-two',function(req,res){
