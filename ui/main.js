65cf17d62d0c46ba3811dd6199809837addac5a8
shibu5555 <shuvamgiri80@gmail.com>
2017-09-05 10:38:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var img=document.getElementById('giri')
+var marginLeft=0;
+function moveRight(){
+	marginLeft=marginLeft+10;
+	img.style.marginLeft + 'px';
+}
+img.onclick=function(){
+	//img.style.marginLeft='100px'
+      var interval=SetInterval(moveRight,100);
+};
